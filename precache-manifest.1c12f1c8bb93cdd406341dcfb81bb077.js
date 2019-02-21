eed3622f7b66f5ead3bb34ddc565d43bc0afafe6
Chris Wacek <chris.wacek@twosixlabs.com>
2019-02-21 17:54:46 -0500

    Make gh-pages branch

---
+++ precache-manifest.1c12f1c8bb93cdd406341dcfb81bb077.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb5c34b133a4a5a62b39b55eb1650588",
+    "url": "/static/media/shield.bb5c34b1.svg"
+  },
+  {
+    "revision": "087a25b1b75ddeadf51c",
+    "url": "/static/css/main.5bd8135e.chunk.css"
+  },
+  {
+    "revision": "45f1850feb5d65d64201fd318cd055e0",
+    "url": "/static/media/shuffle.45f1850f.svg"
+  },
+  {
+    "revision": "6a8a0ebac03bfe25c024",
+    "url": "/static/js/1.6a8a0eba.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ad4c6bed76b72fcfd4dba873322f569",
+    "url": "/static/media/plus-circle.4ad4c6be.svg"
+  },
+  {
+    "revision": "16b9908c5de2082885178c4f9341fd1e",
+    "url": "/static/media/PirataOne-Gloomhaven.16b9908c.ttf"
+  },
+  {
+    "revision": "5f303ebafacbdc6498136b6e7c993c60",
+    "url": "/static/media/Philosopher-Bold.5f303eba.ttf"
+  },
+  {
+    "revision": "59479e31da2a3d02fd161e16577062e2",
+    "url": "/static/media/bless.59479e31.svg"
+  },
+  {
+    "revision": "9664421ce60fbb8830f7a3b14d9c89c9",
+    "url": "/static/media/curse.9664421c.svg"
+  },
+  {
+    "revision": "7a079e57d4d910514a36401667b0e879",
+    "url": "/static/media/shuffle-black.7a079e57.svg"
+  },
+  {
+    "revision": "33f3c32fbaaae4a1cebe09203f6e51ad",
+    "url": "/static/media/minus-circle.33f3c32f.svg"
+  },
+  {
+    "revision": "c15c651ebb2c71b16eae9166e10d8d7b",
+    "url": "/static/media/draw-two.c15c651e.svg"
+  },
+  {
+    "revision": "5873f272dfddd3868e1733b0b25b4d90",
+    "url": "/static/media/attack_mod_back.5873f272.jpg"
+  },
+  {
+    "revision": "a1114c9750bc4567590f6fb519558e27",
+    "url": "/static/media/front.a1114c97.jpg"
+  },
+  {
+    "revision": "1c2fbc7815e9941206a287bf60009c53",
+    "url": "/static/media/back.1c2fbc78.jpg"
+  },
+  {
+    "revision": "8b2be9d77f1a25b7fa17938e96a264de",
+    "url": "/static/media/aoe-4-with-black.8b2be9d7.svg"
+  },
+  {
+    "revision": "aeb051dbf6c9a9db815f54ff6deacfe2",
+    "url": "/static/media/aoe-circle-with-side-black.aeb051db.svg"
+  },
+  {
+    "revision": "c3799cf8b3745f56456bce69fd6faa1c",
+    "url": "/static/media/push.c3799cf8.svg"
+  },
+  {
+    "revision": "443bd9ff0c694e5fb81dfa3cd4ba8412",
+    "url": "/static/media/earth.443bd9ff.svg"
+  },
+  {
+    "revision": "91304f4e92f3848840ab54fceab09374",
+    "url": "/static/media/target.91304f4e.svg"
+  },
+  {
+    "revision": "c963fe2e42f0f23a914676eccb042910",
+    "url": "/static/media/heal.c963fe2e.svg"
+  },
+  {
+    "revision": "1f34cc0381ebe3862a95eabad88aae98",
+    "url": "/static/media/aoe-triangle-3-side-with-corner-black.1f34cc03.svg"
+  },
+  {
+    "revision": "4bfeefc7a62e1082b4e2171f3403ad09",
+    "url": "/static/media/move.4bfeefc7.svg"
+  },
+  {
+    "revision": "b82f281dd42c84601e47c559788f2d9f",
+    "url": "/static/media/stun.b82f281d.svg"
+  },
+  {
+    "revision": "ad30957e31eb04dd5f065e16c48616b2",
+    "url": "/static/media/aoe-triangle-2-side-with-black.ad30957e.svg"
+  },
+  {
+    "revision": "673d53c2f4d6db43f507fca97ffebc8f",
+    "url": "/static/media/fire.673d53c2.svg"
+  },
+  {
+    "revision": "079d79cdabefd485672e127b9837bc03",
+    "url": "/static/media/disarm.079d79cd.svg"
+  },
+  {
+    "revision": "db12f0c32a98712bdb3ff4d8d65b4077",
+    "url": "/static/media/fly.db12f0c3.svg"
+  },
+  {
+    "revision": "b81e97cb5285d668a64cb06f9cff9e9c",
+    "url": "/static/media/elderDrake.special1Area.b81e97cb.svg"
+  },
+  {
+    "revision": "087a25b1b75ddeadf51c",
+    "url": "/static/js/main.087a25b1.chunk.js"
+  },
+  {
+    "revision": "39e1aa1049148ac27ce9c6ecb2540eab",
+    "url": "/static/media/dark.39e1aa10.svg"
+  },
+  {
+    "revision": "fac591e3c321b5669e75c36ffb603b83",
+    "url": "/static/media/air.fac591e3.svg"
+  },
+  {
+    "revision": "abeff9fb25c3cc31026e849ab6153446",
+    "url": "/static/media/jump.abeff9fb.svg"
+  },
+  {
+    "revision": "a7f1b116e1471b680a3e720af74ae9e4",
+    "url": "/static/media/aoe-line-6-with-black.a7f1b116.svg"
+  },
+  {
+    "revision": "96983af10205de5c42c3b92205807def",
+    "url": "/static/media/muddle.96983af1.svg"
+  },
+  {
+    "revision": "24f4accafc8a65cdda6d585695c933ec",
+    "url": "/static/media/sightlessEye.special1Area.24f4acca.svg"
+  },
+  {
+    "revision": "d10777f5e7f4456f28f0e6c6bb36df02",
+    "url": "/static/media/immobilize.d10777f5.svg"
+  },
+  {
+    "revision": "eb11cf13cbae0be5be82a5f854f27d0c",
+    "url": "/static/media/use_element.eb11cf13.svg"
+  },
+  {
+    "revision": "2991b569085c7a04d27309de2357d6f9",
+    "url": "/static/media/loot.2991b569.svg"
+  },
+  {
+    "revision": "7be792dfe0914581a1c5e38a0eb281f6",
+    "url": "/static/media/aoe-circle-with-middle-black.7be792df.svg"
+  },
+  {
+    "revision": "122786948008380f24f614a177e6875f",
+    "url": "/static/media/aoe-triangle-2-side.12278694.svg"
+  },
+  {
+    "revision": "0a6787b3efbf243435aa9129ae38b99c",
+    "url": "/static/media/aoe-circle.0a6787b3.svg"
+  },
+  {
+    "revision": "170dbe3a0f7d555e6887ef99cc8ee3d4",
+    "url": "/static/media/retaliate.170dbe3a.svg"
+  },
+  {
+    "revision": "1ade028be23c8667098ed4261735916b",
+    "url": "/static/media/range.1ade028b.svg"
+  },
+  {
+    "revision": "94f6aac18f7e3a0f01143a0e60b7eaf0",
+    "url": "/static/media/invisibility.94f6aac1.svg"
+  },
+  {
+    "revision": "7c623f77764724aede7acac33c0a7e2d",
+    "url": "/static/media/inoxBodyguard.special1Area.7c623f77.svg"
+  },
+  {
+    "revision": "8acb9b8ecef3675500bb1d25e2ceebb4",
+    "url": "/static/media/strengthen.8acb9b8e.svg"
+  },
+  {
+    "revision": "52753d3cfb0d514dcd181636928d2d30",
+    "url": "/static/media/wound.52753d3c.svg"
+  },
+  {
+    "revision": "a1598f5a051ca44ea571d8fd9a871d26",
+    "url": "/static/media/attack.a1598f5a.svg"
+  },
+  {
+    "revision": "c723bd57ce507e2d0f9fd88656203a1e",
+    "url": "/static/media/sightlessEye.special2Area.c723bd57.svg"
+  },
+  {
+    "revision": "588df0b34e5bbcf0205a28a1d1161201",
+    "url": "/static/media/pierce.588df0b3.svg"
+  },
+  {
+    "revision": "06520bc71c0f1447abee20675c5b6af9",
+    "url": "/static/media/ice.06520bc7.svg"
+  },
+  {
+    "revision": "b39ca9367d23e4614e6a31e36b2698eb",
+    "url": "/static/media/aoe-line-3-with-black.b39ca936.svg"
+  },
+  {
+    "revision": "34db005c43d0c6b86c12575e5ba9a87a",
+    "url": "/static/media/any_element.34db005c.svg"
+  },
+  {
+    "revision": "502d737d8d0e6b91285a2a45a5e8669b",
+    "url": "/static/media/aoe-line-4-with-black.502d737d.svg"
+  },
+  {
+    "revision": "ef434304ecebfa6de270789229b728e6",
+    "url": "/static/media/light.ef434304.svg"
+  },
+  {
+    "revision": "7d7f60b482afb14e571cb82d482d1916",
+    "url": "/static/media/poison.7d7f60b4.svg"
+  },
+  {
+    "revision": "6a8a0ebac03bfe25c024",
+    "url": "/static/css/1.9e807172.chunk.css"
+  },
+  {
+    "revision": "b307651bf4cc6a0f887826f57192d79a",
+    "url": "/index.html"
+  }
+];
